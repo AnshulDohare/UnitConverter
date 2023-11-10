cc55daeed5ba50fc5aa29d85b5abc8189b1ba81e
Anshul Dohare <anshuldohare6904@gmail.com>
2023-11-11 00:14:35 +0530

    Update VolumeConversion.java

--- app/src/main/java/com/example/unitconverter/MainActivity2.java
+++ app/src/main/java/com/example/unitconverter/MainActivity2.java
@@ -5,14 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
